a74d2ce459af3a43ec41a20319624c7dc7cce260
okuzawats <okuzawats@gmail.com>
2021-10-10 06:27:16 +0900

    test give life to cells

--- test/test_board.rb
+++ test/test_board.rb
@@ -2,8 +2,13 @@ require 'minitest/autorun'
 require_relative '../lib/board'
 
 class TestBoard < Minitest::Test
-  def test_initialized
+  def test_initialized_create_cells
     target = Board.new
     assert_equal Board::SIZE**2, target.cells.size
+  end
+
+  def test_initialized_give_life_to_cells
+    target = Board.new
+    assert_equal Board::INITIAL_LIVING_CELL_COUNT, target.cells.select { |cell| cell.isLiving }.size
   end
 end
