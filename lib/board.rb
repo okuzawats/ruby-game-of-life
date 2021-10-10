56a98a564cc79e47f241388901102387fe2d40a6
okuzawats <okuzawats@gmail.com>
2021-10-10 15:17:40 +0900

    random living cells

--- lib/board.rb
+++ lib/board.rb
@@ -2,12 +2,19 @@ require_relative './cell'
 
 class Board
   SIZE = 32
-  INITIAL_LIVING_CELL_COUNT = 16
-
-  attr_reader :cells
+  INITIAL_LIVING_CELLS = 16
 
   def initialize
-    @cells = Array.new(SIZE**2) { Cell.new }
-    @cells.sample(INITIAL_LIVING_CELL_COUNT).each { |cell| cell.birth }
+    @cells = (Array.new(INITIAL_LIVING_CELLS) { true } + Array.new(SIZE**2 - INITIAL_LIVING_CELLS) { false })
+      .shuffle
+      .map { |is_living| Cell.new(is_living) }
+  end
+
+  def cells_size
+    @cells.size
+  end
+
+  def living_cells_size
+    @cells.select { |cell| cell.alive? }.size
   end
 end
--- test/test_board.rb
+++ test/test_board.rb
@@ -4,11 +4,11 @@ require_relative '../lib/board'
 class TestBoard < Minitest::Test
   def test_initialized_create_cells
     target = Board.new
-    assert_equal Board::SIZE**2, target.cells.size
+    assert_equal Board::SIZE**2, target.cells_size
   end
 
   def test_initialized_give_life_to_cells
     target = Board.new
-    assert_equal Board::INITIAL_LIVING_CELL_COUNT, target.cells.select { |cell| cell.alive? }.size
+    assert_equal Board::INITIAL_LIVING_CELLS, target.living_cells_size
   end
 end
