e8114ef826fc1543855223fa94babfeb04c137ae
okuzawats <okuzawats@gmail.com>
2021-10-11 08:44:10 +0900

    define cell_binder

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 require_relative './cell'
+require_relative './cell_binder'
 
 class Board
   WIDTH = 32
@@ -8,29 +9,8 @@ class Board
   INITIAL_DEAD_CELLS = CELLS - INITIAL_LIVING_CELLS
 
   def initialize
-    @cells = (Array.new(INITIAL_LIVING_CELLS, true) + Array.new(INITIAL_DEAD_CELLS, false))
-      .shuffle
-      .map { |is_living| Cell.new(is_living) }
-    bind_cells
-  end
-
-  def bind_cells
-    (0...HEIGHT).each { |i|
-      (0...WIDTH).each { |j|
-        index = i * WIDTH + j
-        i - 1 >= 0 || i + 1 < HEIGHT || j - 1 >= 0 || j + 1 < WIDTH
-        @cells[index].tap do |cell|
-          cell.add_neighbor @cells[index - WIDTH - 1] if (i - 1 >= 0 && j - 1 >= 0)
-          cell.add_neighbor @cells[index - WIDTH] if (i - 1 >= 0)
-          cell.add_neighbor @cells[index - WIDTH + 1] if (i - 1 >= 0 && j + 1 < WIDTH)
-          cell.add_neighbor @cells[index - 1] if (j - 1 >= 0)
-          cell.add_neighbor @cells[index + 1] if (j + 1 < WIDTH)
-          cell.add_neighbor @cells[index + WIDTH - 1] if (i + 1 < HEIGHT && j - 1 >= 0)
-          cell.add_neighbor @cells[index + WIDTH] if (i + 1 < HEIGHT)
-          cell.add_neighbor @cells[index + WIDTH + 1] if (i + 1 < HEIGHT && j + 1 < WIDTH)
-        end
-      }
-    }
+    cell_binder = CellBinder.new(INITIAL_LIVING_CELLS, INITIAL_DEAD_CELLS)
+    @cells = cell_binder.bind_cells(WIDTH, HEIGHT)
   end
 
   def update_cells
