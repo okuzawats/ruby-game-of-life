d3a222130312ed25e5e702c70c2f8080904f3e4b
okuzawats <okuzawats@gmail.com>
2021-10-11 21:33:19 +0900

    fix test_board

--- test/test_board.rb
+++ test/test_board.rb
@@ -1,14 +1,16 @@
 require 'minitest/autorun'
 require_relative '../lib/board'
+require_relative '../lib/dimens'
+require_relative '../lib/envs'
 
 class TestBoard < Minitest::Test
   def test_initialized_create_cells
     target = Board.new
-    assert_equal Board::CELLS, target.cells_size
+    assert_equal Dimens::WIDTH * Dimens::HEIGHT, target.cells_size
   end
 
   def test_initialized_give_life_to_cells
     target = Board.new
-    assert_equal Board::INITIAL_LIVING_CELLS, target.living_cells_size
+    assert_equal Envs::INITIAL_LIVING_CELLS, target.living_cells_size
   end
 end
