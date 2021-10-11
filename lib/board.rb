8be1c2f28245a6971d5704bcc5fa5c6ae07a784a
okuzawats <okuzawats@gmail.com>
2021-10-11 20:45:22 +0900

    define Dimens

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,17 @@
 require_relative './board_displayer'
 require_relative './cell'
 require_relative './cell_binder'
+require_relative './dimens'
 
 class Board
-  WIDTH = 32
-  HEIGHT = 32
-  CELLS = WIDTH * HEIGHT
   INITIAL_LIVING_CELLS = 128
   INITIAL_DEAD_CELLS = CELLS - INITIAL_LIVING_CELLS
 
   def initialize
+    dimens = Dimens.new
     cell_binder = CellBinder.new(INITIAL_LIVING_CELLS, INITIAL_DEAD_CELLS)
-    @cells = cell_binder.bind_cells(WIDTH, HEIGHT)
-    @displayer = BoardDisplayer.new(@cells, WIDTH, HEIGHT)
+    @cells = cell_binder.bind_cells(dimens.width, dimens.height)
+    @displayer = BoardDisplayer.new(@cells, dimens.width, dimens.height)
   end
 
   def update_cells
---
+++ lib/dimens.rb
@@ -0,0 +1,16 @@
+class Dimens
+  WIDTH = 32
+  HEIGHT = 32
+
+  def width
+    WIDTH
+  end
+
+  def height
+    HEIGHT
+  end
+
+  def size
+    WIDTH * HEIGHT
+  end
+end
