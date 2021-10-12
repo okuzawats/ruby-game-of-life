c57b09b5b9f357100c2e13ff10652b542529ca7c
okuzawats <okuzawats@gmail.com>
2021-10-12 21:53:37 +0900

    define CLEAR as a constant

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
 require_relative './lib/board'
 
+CLEAR = 'clear'
 ONE_SECOND = 1
 
 board = Board.new
 
 while true
-  system('clear')
+  system CLEAR
   board.display
   board.update
   sleep ONE_SECOND
